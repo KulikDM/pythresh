801603fd77118e52c45986cfb033b7f2ee246eeb
KulikDM <74867610+KulikDM@users.noreply.github.com>
2022-05-29 21:10:14 +0200

    setup

---
+++ pythresh/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import thresholds
+# TODO: add version information here
+
+__all__ = ['thresholds']
---
+++ pythresh/version.py
@@ -0,0 +1,7 @@
+"""
+``pythresh`` is a python toolbox for outlier detection thresholding
+"""
+# Based on NiLearn package
+# License: simplified BSD
+
+__version__ = '0.1.0'  # pragma: no cover
