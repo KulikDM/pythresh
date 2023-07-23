2486671ceb66f25a8c4d33c4a9a4c6b1ffad54a3
KulikDM <rogerplanck8@gmail.com>
2023-07-23 11:46:47 +0200

    Make GAMGMM tests parallel

--- pythresh/test/test_gamgmm.py
+++ pythresh/test/test_gamgmm.py
@@ -1,9 +1,12 @@
+import os
 import sys
 import unittest
+from itertools import product
 from os.path import dirname as up
 
 # noinspection PyProtectedMember
 import numpy as np
+from joblib import Parallel, delayed
 from numpy.testing import assert_equal
 from pyod.models.iforest import IForest
 from pyod.models.knn import KNN
@@ -17,6 +20,26 @@ from pythresh.thresholds.gamgmm import GAMGMM
 
 path = up(up(up(__file__)))
 sys.path.append(path)
+
+sys.stdout = open(os.devnull, 'w')
+
+
+def tester(y_train, scores, verbose, n_contam, draws,
+           p0, phigh, high_gamma, K):
+
+    thres = GAMGMM(n_contaminations=n_contam, n_draws=draws,
+                   p0=p0, phigh=phigh, high_gamma=high_gamma,
+                   K=K, skip=True, verbose=verbose)
+
+    pred_labels = thres.eval(scores)
+    assert (thres.thresh_ is not None)
+
+    assert_equal(pred_labels.shape, y_train.shape)
+
+    if (not np.all(pred_labels == 0)) & (not np.all(pred_labels == 1)):
+
+        assert (pred_labels.min() == 0)
+        assert (pred_labels.max() == 1)
 
 
 class TestGAMGMM(unittest.TestCase):
@@ -40,6 +63,8 @@ class TestGAMGMM(unittest.TestCase):
             clf.fit(self.X_train)
             multiple_scores.append(clf.decision_scores_)
 
+        multiple_scores = np.vstack(multiple_scores).T
+
         self.all_scores = [scores, multiple_scores]
 
         self.n_contaminations = [200, 1000, 10000]
@@ -58,33 +83,12 @@ class TestGAMGMM(unittest.TestCase):
 
     def test_prediction_labels(self):
 
-        for scores in self.all_scores:
+        # Create an iterable of all the loop variables
+        all_loop_variables = [self.all_scores, self.verbose, self.n_contaminations,
+                              self.n_draws, self.p0, self.phigh, self.high_gamma, self.K]
 
-            for verbose in self.verbose:
+        # Get all combinations of loop variables
+        all_combinations = list(product(*all_loop_variables))
 
-                for n_contam in self.n_contaminations:
-
-                    for draws in self.n_draws:
-
-                        for p0 in self.p0:
-
-                            for phigh in self.phigh:
-
-                                for high_gamma in self.high_gamma:
-
-                                    for K in self.K:
-
-                                        self.thres = GAMGMM(n_contaminations=n_contam, n_draws=draws,
-                                                            p0=p0, phigh=phigh, high_gamma=high_gamma,
-                                                            K=K, skip=True, verbose=verbose)
-
-                                        pred_labels = self.thres.eval(scores)
-                                        assert (self.thres.thresh_ is not None)
-
-                                        assert_equal(
-                                            pred_labels.shape, self.y_train.shape)
-
-                                        if (not np.all(pred_labels == 0)) & (not np.all(pred_labels == 1)):
-
-                                            assert (pred_labels.min() == 0)
-                                            assert (pred_labels.max() == 1)
+        Parallel(n_jobs=-1)(delayed(tester)(self.y_train, *args)
+                            for args in all_combinations)
